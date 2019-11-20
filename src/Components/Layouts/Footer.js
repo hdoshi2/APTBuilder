d2f45a51319092f932017584f719f1e881b8ac97
Hari.Doshi <hdoshi2@gmail.com>
2019-11-19 23:08:13 -0500

    feat: added header footer components

---
+++ src/Components/Layouts/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return(
+    <div className="App">
+    <header className="App-header">
+      <div>
+        Footer
+      </div>
+    </header>
+  </div>
+  )
+}
+
+export default Footer
---
+++ src/Components/Layouts/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          Headerz
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
