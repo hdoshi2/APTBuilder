7ae29299bb4bb97df7b4d497eb42d049aa154b4d
Hari.Doshi <hdoshi2@gmail.com>
2019-12-08 21:21:58 -0500

    added dialog box for new items

--- src/Components/Exercises/Dialogs/Create.js
+++ src/Components/Exercises/Dialogs/Create.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Button,
   TextField,
@@ -6,35 +6,62 @@ import {
   DialogActions,
   DialogContent,
   DialogContentText,
-  DialogTitle
-} from '@material-ui/core/Button';
+  DialogTitle,
+  Fab
+} from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
 
 
-export default props =>
-<>
-  <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-  <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
-  <DialogContent>
-    <DialogContentText>
-      To subscribe to this website, please enter your email address here. We will send updates
-      occasionally.
-    </DialogContentText>
-    <TextField
-      autoFocus
-      margin="dense"
-      id="name"
-      label="Email Address"
-      type="email"
-      fullWidth
-    />
-  </DialogContent>
-  <DialogActions>
-    <Button onClick={handleClose} color="primary">
-      Cancel
-    </Button>
-    <Button onClick={handleClose} color="primary">
-      Subscribe
-    </Button>
-  </DialogActions>
-  </Dialog>
-</>
+export default class Create extends Component {
+
+  state = {
+    open: false
+  }
+
+  handleToggle = () => {
+    this.setState({
+      open: !this.state.open
+    })
+  }
+
+
+  render() {
+    const { open } = this.state;
+
+    return (
+      <>
+        <Button size="small" color="secondary" aria-label="add" onClick={this.handleToggle} style={{maxWidth: '30px'}}>
+          <Fab color="primary" aria-label="add">
+            <AddIcon />
+          </Fab>
+        </Button>
+        
+        <Dialog
+          open={open}
+          aria-labelledby="form-dialog-title"
+          onClose={this.handleToggle}
+        >
+          <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Please fill out the form below.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Email Address"
+              type="email"
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button color="primary" variant="contained">
+              Create
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </>
+    )
+  }
+}
--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, {Component}from 'react';
 import {AppBar, Toolbar, Typography, IconButton, Button} from '@material-ui/core'
+import Create from '../Exercises/Dialogs/Create'
 
-
-function Header() {
-  return (
-<AppBar position="static">
-  <Toolbar>
-    <IconButton edge="start" color="inherit" aria-label="menu">
-      {/* <MenuIcon /> */}
-    </IconButton>
-    <Typography variant="h6">
-      News
-    </Typography>
-    <Button color="inherit">Login</Button>
-  </Toolbar>
-</AppBar>
-  );
+class Header extends Component{
+  render() {
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+          </IconButton>
+          <Typography variant="h6" color="inherit" style={{flex: 1}}>
+            News
+          </Typography>
+          <Create/>
+        </Toolbar>
+      </AppBar>
+    )
+  }
 }
 
 export default Header;
